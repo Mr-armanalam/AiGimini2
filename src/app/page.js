47651f7a18ab3dec266a70b839b9fc22b2a247c7
Mr-armanalam <armanalam78578@gmail.com>
2024-07-27 12:00:32 +0530

    Optional Question now set

--- src/app/page.js
+++ src/app/page.js
@@ -5,34 +5,49 @@ import { PiUserBold } from "react-icons/pi";
 import { SiGooglebard } from "react-icons/si";
 import { BsArrowReturnLeft } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
-import { CgFormatSlash } from "react-icons/cg";
-import Link from "next/link";
-import { FaGithub } from "react-icons/fa";
+import { Navbar } from "./components/navbar.jsx";
 
 export default function Home() {
+
   const [userInput, setUserInput] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
   const [usersubmit, setUsersubmit] = useState(false);
   const [storeQuestion, setStoreQuestion] = useState([]);
+  const [selectedQuestion, setSlectedQuestion] = useState([]);
   const [userClick, setUserClick] = useState(false);
-  // const Question = [
-  //   "Syntax Errors",
-  //   "Logical Errors",
-  //   " Debugging of code",
-  //   "Reference Errors",
-  // ];
+
+  const Question = [
+    "Syntax Errors",
+    "Logical Errors",
+    " Debugging of code",
+    "Reference Errors",
+  ];
 
   const inputHandle = (e) => {
     setUserInput(e.target.value);
     setUserClick(true);
   };
 
-  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
+  const optionalQhandler = (indx) => {
+    setSlectedQuestion(selectedQuestion.push(Question[indx]));
+    console.log(selectedQuestion);
+    aiResponse();
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (!userInput) return;
-    setStoreQuestion([...storeQuestion, userInput]);
+    aiResponse();
+  };
+
+  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
+  const aiResponse = async () => {
+    if (userClick) {
+      setStoreQuestion([...storeQuestion, userInput]);
+    } else {
+      setUserClick(true);
+      setStoreQuestion([...storeQuestion, selectedQuestion]);
+    }
+
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const chat = model.startChat({
       history: [
@@ -58,42 +73,19 @@ export default function Home() {
       },
     });
 
-    const msg = userInput;
+    const msg = selectedQuestion ? userInput : storeQuestion;
     const result = await chat.sendMessage(msg);
     const response = await result.response;
     const text = response.text();
-    console.log(text);
     setChatHistory([...chatHistory, text]);
-    //setUserInput("");
+    setSlectedQuestion([]);
     setUsersubmit(true);
   };
 
   return (
     <>
-      <div className="flex fixed z-50 justify-between w-full h-[64px] items-center bg-[#09090B] border pl-2 border-zinc-800 rounded">
-        <div className="flex items-center">
-          <Link href="/">
-            <RiNextjsFill size={36} />
-          </Link>
-          <CgFormatSlash size={36} color="gray" />
-          <button className="text-zinc-100 font-bold">Login</button>
-        </div>
-
-        <div className="flex">
-          <Link href="https://github.com/Mr-armanalam">
-            <button className="bg-black flex flex-row justify-center items-center rounded-md border border-zinc-800 w-[150px] font-bold text-sm h-[42px] mr-2">
-              <FaGithub style={{ marginRight: "14px" }} />
-              Github
-            </button>
-          </Link>
-
-          <button className="rounded-md border border-zinc-800 w-[150px] h-[42px] bg-white text-sm font-bold mr-4 text-zinc-800">
-            My AiChatbot
-          </button>
-        </div>
-      </div>
-
-      <div className="container h-screen relative mx-auto w-[44rem] pt-20">
+      <Navbar />
+      <div className="container h-screen relative mx-auto md:w-[44rem] sm:w-[100%] pt-20">
         {userClick ? (
           <>
             <div className="overflow-auto flex flex-col-reverse scrollbar max-h-[60vh]">
@@ -129,39 +121,44 @@ export default function Home() {
           </>
         ) : (
           <>
-            <div className="general_q_container  ">
-              <div className="bg-[#09090B] p-8 rounded-lg border border-gray-800 mb-2 ">
+            <div className="general_q_container relative">
+              <div className="bg-[#09090B] p-8 sm:w-full rounded-lg border border-gray-800 mb-2 ">
                 <p className="text-white text-lg font-semibold pb-4 ">
-                  Welcome to Next.js AI Chatbot!
+                  Welcome to AI Code Assistant Chatbot!
                 </p>
                 <p className="pb-2 text-zinc-400">
-                  This is an open source AI chatbot app template built with
-                  Next.js , the Vercel AI SDK , and Vercel KV .
+                  This is an innovative tool designed to enhance and correct the
+                  code & provide better coding experience for developers.
                 </p>
                 <p className="text-zinc-400 ">
-                  It uses React Server Components to combine text with
-                  generative UI as output of the LLM. The UI state is synced
-                  through the SDK so the model is aware of your interactions as
-                  they happen.
+                  Intelligent Code Suggestions, Code Generation Learning Mode
+                  this chatbot assists programmers in various ways, from
+                  answering coding queries to suggesting code snippets
                 </p>
               </div>
-              <div className="flex flex-row justify-between flex-wrap ">
+              <div className="flex flex-row sm:justify-center md:justify-between flex-wrap overflow-auto scrollbar max-h-[40vh] ">
                 <div
-                  // onClick={setStoreQuestion([...storeQuestion, Question[0]])}
-                  className="bg-[#09090B] p-4 rounded-lg border border-gray-800 mb-2 w-[348px] "
+                  onClick={() => optionalQhandler(0)}
+                  className="bg-[#09090B] p-4 cursor-pointer rounded-lg border border-gray-800 mb-2 w-[348px] "
                 >
                   <p className=" text-white text-base font-semibold ">
                     What are the
                   </p>
                   <p className="text-zinc-400">Syntax Errors in programming</p>
                 </div>
-                <div className="bg-[#09090B] p-4 rounded-lg border border-gray-800 mb-2 w-[348px]  ">
+                <div
+                  onClick={() => optionalQhandler(1)}
+                  className="bg-[#09090B] p-4 cursor-pointer rounded-lg border border-gray-800 mb-2 w-[348px]  "
+                >
                   <p className=" text-white text-base font-semibold ">
                     What are the
                   </p>
                   <p className="text-zinc-400">Logical Errors in programming</p>
                 </div>
-                <div className="bg-[#09090B]  p-4 rounded-lg border border-gray-800 mb-2 w-[348px] ">
+                <div
+                  onClick={() => optionalQhandler(2)}
+                  className="bg-[#09090B] cursor-pointer p-4 rounded-lg border border-gray-800 mb-2 w-[348px] "
+                >
                   <p className=" text-white text-base font-semibold ">
                     What are the
                   </p>
@@ -169,7 +166,10 @@ export default function Home() {
                     debuging of code in programming
                   </p>
                 </div>
-                <div className="bg-[#09090B]  p-4 rounded-lg border border-gray-800 mb-2 w-[348px] ">
+                <div
+                  onClick={() => optionalQhandler(3)}
+                  className="bg-[#09090B] cursor-pointer p-4 rounded-lg border border-gray-800 mb-2 w-[348px] "
+                >
                   <p className=" text-white text-base font-semibold ">
                     What are the
                   </p>
@@ -181,8 +181,7 @@ export default function Home() {
             </div>
           </>
         )}
-
-        <div className="aigen p-4 absolute bottom-0 bg-[#09090B] rounded-lg border border-gray-800 mt-4 ">
+        <div className="aigen w-full p-4 absolute bottom-0 bg-[#09090B] rounded-lg border border-gray-800 mt-4 ">
           <div className="px-4 flex items-center h-[4rem] rounded-md border border-gray-800 ">
             <form className="flex flex-row justify-self-end justify-between">
               <label htmlFor="question">
@@ -193,13 +192,13 @@ export default function Home() {
               <input
                 type="text"
                 id="question"
-                className="w-[500px]  me-16 px-4 bg-transparent py-2 text-white border-none rounded-md focus:outline-none focus:border-zinc-500"
+                className="w-[30rem]  me-16 px-4 bg-transparent py-2 text-white border-none rounded-md focus:outline-none focus:border-zinc-500"
                 placeholder="Type your message..."
                 value={userInput}
                 onChange={inputHandle}
               />
               <button
-                className="w-[36px] flex h-[36px] items-center justify-center text-base text-white bg-transparent rounded-lg border border-gray-800 hover:bg-zinc-600 focus:outline-none"
+                className="w-[36px] flex h-[36px] items-center justify-center  text-white bg-transparent rounded-lg border border-gray-800 hover:bg-zinc-600 focus:outline-none"
                 type="submit"
                 onClick={handleSubmit}
               >
